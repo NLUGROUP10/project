ab14b837f3ee6b763fd9bc2831d6036f8ed0962a
Tianshu Wang <twang650@gatech.edu>
2021-04-24 02:33:03 -0400

    fix a bug of yacc

--- tree_utiles/lex.py
+++ tree_utiles/lex.py
@@ -1,4 +1,5 @@
 import json
+from node import Node
 tokens = (
     'NAME','INT','FLOAT',
     'PLUS','MINUS','TIMES','DIVIDE','EQUALS','POWER',
@@ -48,62 +49,10 @@ lexer = lex.lex()
 precedence = (
     ('left','PLUS','MINUS'),
     ('left','TIMES','DIVIDE'),
+    ('left','POWER'),
     ('right','UMINUS'),
     )
-class Node:
-    def __init__(self,val,left=None,right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-    def display(self):
-        lines, *_ = self._display_aux()
-        for line in lines:
-            print(line)
 
-    def _display_aux(self):
-        """Returns list of strings, width, height, and horizontal coordinate of the root."""
-        # No child.
-        if self.right is None and self.left is None:
-            line = '%s' % self.val
-            width = len(line)
-            height = 1
-            middle = width // 2
-            return [line], width, height, middle
-
-        # Only left child.
-        if self.right is None:
-            lines, n, p, x = self.left._display_aux()
-            s = '%s' % self.val
-            u = len(s)
-            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
-            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
-            shifted_lines = [line + u * ' ' for line in lines]
-            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
-
-        # Only right child.
-        if self.left is None:
-            lines, n, p, x = self.right._display_aux()
-            s = '%s' % self.val
-            u = len(s)
-            first_line = s + x * '_' + (n - x) * ' '
-            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
-            shifted_lines = [u * ' ' + line for line in lines]
-            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
-
-        # Two children.
-        left, n, p, x = self.left._display_aux()
-        right, m, q, y = self.right._display_aux()
-        s = '%s' % self.val
-        u = len(s)
-        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
-        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
-        if p < q:
-            left += [n * ' '] * (q - p)
-        elif q < p:
-            right += [m * ' '] * (p - q)
-        zipped_lines = zip(left, right)
-        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
-        return lines, n + m + u, max(p, q) + 2, n + u // 2
 # dictionary of names (for storing variables)
 names = { 'x':'x' }
 res_head = None
@@ -115,7 +64,7 @@ def p_equation_assign(p):
     global res_head
     res_head = p[0]
 
-
+#todo 优先级问题
 
 def p_expression_binop(p):
     '''expression : expression PLUS expression
@@ -169,8 +118,8 @@ def p_error(p):
 import ply.yacc as yacc
 import collections
 yacc.yacc(debug=True)
-# test_data = "x = -(-12.99/4)**-3+(-1/22+2*3)"
-test_data = "1.1**4"
+test_data = "x = -(-12.99/4)**-3*2+1"
+# test_data = "1.1**4"
 yacc.parse(test_data)
 def traverse(root):
     r = []
@@ -196,3 +145,4 @@ def printTree(node, level=0):
 # printTree(res_head)
 
 res_head.display()
+res_head.pre_order_traverse(res_head)
